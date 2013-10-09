75da7ea965eb6d93cb9be738e1eeefbe9ce6fb2a
azee <azee@yandex-team.ru>
2013-10-10 02:26:02 +0400

    Odd numbers

---
+++ src/main/java/math/OddNumbers.java
@@ -0,0 +1,16 @@
+package math;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: azee
+ * Date: 10/10/13
+ * Time: 2:24 AM
+ */
+public class OddNumbers {
+    public static void main(String[] args) {
+        int depth = 100;
+        for (int i = 1; i <= depth; i+=2){
+            System.out.println(i);
+        }
+    }
+}
