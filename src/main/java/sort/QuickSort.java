bf4dcc6cb8e1c17d5d847a1d658f1262c00cbaf1
azee <azee@yandex-team.ru>
2013-10-10 14:38:57 +0400

    Qsort with comments

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -18,19 +18,40 @@ public class QuickSort {
     public static void qSort(int[] array, int low, int high) {
         int i = low;
         int j = high;
-        int x = array[(low+high)/2];
-        do {
-            while(array[i] < x) ++i;
-            while(array[j] > x) --j;
-            if(i <= j){
+
+        // Get the pivot element from the middle of the list
+        int pivot = array[low + (high-low)/2];
+
+        // Divide into two lists
+        while (i <= j) {
+            // If the current value from the left list is smaller then the pivot
+            // element then get the next element from the left list
+            while (array[i] < pivot) {
+                i++;
+            }
+            // If the current value from the right list is larger then the pivot
+            // element then get the next element from the right list
+            while (array[j] > pivot) {
+                j--;
+            }
+
+            // If we have found a values in the left list which is larger then
+            // the pivot element and if we have found a value in the right list
+            // which is smaller then the pivot element then we exchange the
+            // values.
+            // As we are done we can increase i and j
+            if (i <= j) {
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
-                i ++ ; j --;
+                i++;
+                j--;
             }
-        } while(i <= j);
-
-        if(low < j) qSort(array, low, j);
-        if(i < high) qSort(array, i, high);
+        }
+        // Recursion
+        if (low < j)
+            qSort(array, low, j);
+        if (i < high)
+            qSort(array, i, high);
     }
 }
