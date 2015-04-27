fed636170bd6218aee5a7c8c1e4bfc96b94706f1
Aziz Namazov <azee@77.88.2.156-spb.dhcp.yndx.net>
2015-04-27 16:35:23 +0300

    KMP

---
+++ src/main/java/HashMapThreadAware.java
@@ -0,0 +1,5 @@
+/**
+ * Created by azee on 24.04.15.
+ */
+public class HashMapThreadAware {
+}
--- src/main/java/strings/KMP.java
+++ src/main/java/strings/KMP.java
@@ -4,5 +4,50 @@ package strings;
  * Created by azee on 14.01.15.
  */
 public class KMP {
-    //ToDo: implement
+    private static int[] failure;
+
+    public static int find(String text, String toFind) {
+        /** pre construct failure array for a pattern **/
+        failure = new int[toFind.length()];
+        fail(toFind);
+        return posMatch(text, toFind);
+    }
+
+    /** Failure function for a pattern **/
+    private static void fail(String pattern)
+    {
+        int n = pattern.length();
+        failure[0] = -1;
+        for (int j = 1; j < n; j++)
+        {
+            int i = failure[j - 1];
+            while ((pattern.charAt(j) != pattern.charAt(i + 1)) && i >= 0)
+                i = failure[i];
+            if (pattern.charAt(j) == pattern.charAt(i + 1))
+                failure[j] = i + 1;
+            else
+                failure[j] = -1;
+        }
+    }
+
+    /** Function to find match for a pattern **/
+    private static int posMatch(String text, String pattern)
+    {
+        int i = 0, j = 0;
+        int lens = text.length();
+        int lenp = pattern.length();
+        while (i < lens && j < lenp)
+        {
+            if (text.charAt(i) == pattern.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else if (j == 0)
+                i++;
+            else
+                j = failure[j - 1] + 1;
+        }
+        return ((j == lenp) ? (i - lenp) : -1);
+    }
 }
